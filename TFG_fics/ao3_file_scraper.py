3f658a2c605bec43f86d8fa6e4fc51808080fb90
mariaGnlz <mariakg@protonmail.com>
2020-04-12 00:54:41 +0200

    Fixed bugs. ao3_file_scraper now writes a file with the links of the files that coudln't be downloaded for some reason

--- TFG_fics/ao3_file_scraper.py
+++ TFG_fics/ao3_file_scraper.py
@@ -12,14 +12,28 @@ HTML_FIC_LISTING_PATH = '/home/maria/Documents/Fanfic_ontology/html_fic_paths.tx
 def get_work_links_from_file():
 	link_file = open('fic_work_links.txt', 'r')
 
-	work_links = [line[:-1] for line in link_file.readlines()]
+	work_links = [line[:-1] for line in link_file.readlines()] #take out the \n at the end of the line
+	link_file.close()
 	
 	return work_links 
 
+def write_out_file(link, reason, index):
+	out_file = open(HTML_FICS_PATH+'out.txt', 'a')
+	out_file.write(link+'\nReason: '+reason+' Index: '+str(index)+'\n')
+	out_file.close()
+
+
 def get_html_link(page):
 	soup = BeautifulSoup(page.content, 'html.parser')
-	all_links = (soup.find(class_='download')).find_all('a')
-	html_link = all_links[len(all_links)-1]
+			
+
+	if soup.find('title').text == '\n          New\n          Session\n        |\n        Archive of Our Own\n    ':
+		#the work is private and shouldn't be downloaded
+		html_link=''
+	
+	else:				
+		all_links = (soup.find(class_='download')).find_all('a')
+		html_link = all_links[len(all_links)-1]
 
 	return html_link
 
@@ -43,6 +57,8 @@ def download_works_in_range(work_links, start, end):
 				print('Deleted work at '+work_links[i])
 				deleted = True
 				num_deleted += 1
+				write_out_file(work_links[i], 'deleted', i)
+
 				break
 
 			else: break
@@ -50,37 +66,47 @@ def download_works_in_range(work_links, start, end):
 		if not deleted: #only continue with iteration if the work is still online
 
 			html_link = get_html_link(page)
-	
-			download_link = 'https://archiveofourown.org'+html_link['href']
-			#print(download_link) #debug
+
+			if html_link == '': #this work is private
+				num_deleted += 1
+				write_out_file(work_links[i], 'private', i)
+				print('Private work at '+work_links[i])
+				
+			else:
+				download_link = 'https://archiveofourown.org'+html_link['href']
+				#print(download_link) #debug
 			
-			print('Downloading ',i,'of ',end,'. . .')
+				print('Downloading ',i,'of ',end,'. . .')
 
-			try:
-				fanfic_path, _ = urllib.request.urlretrieve(download_link, HTML_FICS_PATH+'gomensfanfic_'+str(i)+'.html')
-				#print(fanfic_path) #debug
+				try:
+					fanfic_path, _ = urllib.request.urlretrieve(download_link, HTML_FICS_PATH+'gomensfanfic_'+str(i)+'.html')
+					#print(fanfic_path) #debug
 
-				#Write path to fanfic on html_fic_paths.txt
-				html_list = open(HTML_FIC_LISTING_PATH, 'w')
-				html_list.write(fanfic_path+'\n')
-				html_list.close()
+					#Write path to fanfic on html_fic_paths.txt
+					html_list = open(HTML_FIC_LISTING_PATH, 'a')
+					html_list.write(fanfic_path+'\n')
+					html_list.close()
+	
+				except HTTPError as e:
+					print('HTTPError ',e.code,': ',e.reason,'\nIn fanfic number ',i)
+					if e.code == 429: #Too Many Requests
+						i-=1 #Will re-attempt iteration from the beginning
 
-			except HTTPError as e:
-				print('HTTPError ',e.code,': ',e.reason,'\nIn fanfic number ',i)
-				if e.code == 429: #Too Many Requests
-					i-=1 #Will re-attempt iteration from the beginning
+				except ContentTooShortError as e:
+					print('ContentTooShortError ',e.code,':',e.reason,'\nIn fanfic number ',i)
+					write_out_file(work_links[i], e.reason, i)
 
-			except ContentTooShortError as e:
-				print('ContentTooShortError ',e.code,':',e.reason,'\nIn fanfic number ',i)
+				except URLError as e:
+					print('URLError ',e.code,': ',e.reason,'\nIn fanfic number ',i)
+					write_out_file(work_links[i], e.reason, i)
 
-			except URLError as e:
-				print('URLError ',e.code,': ',e.reason,'\nIn fanfic number ',i)
+				except (IOError, OSError) as e:
+					print('IOError/OSerror ',e.errno,': ',e.strerror,'\nIn fanfic number ',i)
+					write_out_file(work_links[i], e.strerror, i)
 
-			except (IOError, OSError) as e:
-				print('IOError ',e.code,': ',e.reason,'\nIn fanfic number ',i)
-
-			except Error as e:
-				print('Error ',e.code,': '.e.reason,'\nIn fanfic number ',i)
+				except Error as e:
+					print('Error ',e.errno,': ',e.strerror,'\nIn fanfic number ',i)
+					write_out_file(work_links[i], e.strerror, i)
 
 		
 		#end if not deleted
@@ -92,12 +118,14 @@ def download_works_in_range(work_links, start, end):
 	return num_deleted, num_fics #return number of deleted fics and fics successfully downloaded
 
 work_links = get_work_links_from_file()
+initial_num = len((open(HTML_FIC_LISTING_PATH, 'r')).readlines())
 
 start = time.time()
-num_deleted, num_fics = download_works_in_range(work_links, 751, 2000)
-#_, num_fics = download_works_in_range(work_links, 0, 1) #debug
+#num_deleted, num_fics = download_works_in_range(work_links, 2000, 4000)
+num_deleted, num_fics = download_works_in_range(work_links, 451, 1000)
+#num_deleted, num_fics = download_works_in_range(work_links, 0, 20) #debug
 end = time.time()
 
-print('Successfully downloaded ',num_fics,' fanfics in ',(end-start)/60,' minutes to '+HTML_FICS_PATH)
+print('Successfully downloaded ',(num_fics-initial_num),' fanfics in ',(end-start)/60,' minutes to '+HTML_FICS_PATH)
 print('Deleted fics: ',num_deleted)
 
