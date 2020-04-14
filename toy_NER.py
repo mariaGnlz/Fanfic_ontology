981d6596e97d72ad7db17b4793cfa7a00e87474e
mariaGnlz <mariakg@protonmail.com>
2020-04-14 20:06:09 +0200

    Renamed some files. Started changes so toy_NER will dumpt its output into a CSV file

--- toy_NER.py
+++ toy_NER.py
@@ -4,98 +4,110 @@
 #Toy version of an entity recognition process
 
 import nltk, re, pprint
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.tag import pos_tag
+from toy_chunkers import UnigramChunker
+import pandas
 
 ### VARIABLES ###
-TXT_FIC_LISTING_PATH = '/home/maria/Documents/Fanfic_ontology/txt_fic_paths.txt'
+FIC_LISTING_PATH = '/home/maria/Documents/Fanfic_ontology/trial_fic_paths.txt'
 
 ### FUNCTIONS ###
 
-def ie_preprocess(document): #divide raw text into words and tag them	
-	#sentences = nltk.sent_tokenize(document) #sentence segmentation
-	#sentences = [word_tokenize(sent) for sent in sentences] #tokenization	
-	#sentences = [pos_tag(sent) for sent in sentences] #POS tagging
+def preprocess_fics(fic_list): #divide raw text into words and tag them	
+	fic_sentences = [sent_tokenize(fic) for fic in fic_list]	
+	processed_fics = [[pos_tag(word_tokenize(sent)) for sent in fic] for fic in fic_sentences]
 
-	sentences = pos_tag(word_tokenize(document))
-	return sentences
+	return processed_fics
 
 def get_processed_fanfics(): #gets the paths to the fics, opens them
                    #and concatenates their textin a string
-	fic_paths = []
-	fic_paths_file = open(TXT_FIC_LISTING_PATH, 'r')
-
-	for path in fic_paths_file.readlines():
-		fic_paths.append(path[:-1])
-
-	fic_paths_file.close()
+	paths_file = open(FIC_LISTING_PATH, 'r')
+	fic_paths = [line[:-1] for line in paths_file.readlines()]
+	paths_file.close()
 	
-	fic_text=''
+	fic_list = []
 	for path in fic_paths:
 		txt_file = open(path, 'r')
-		fic_text=fic_text+txt_file.read()
+		fic_list.append(txt_file.read())
 		txt_file.close()
 
-	fic_text = ie_preprocess(fic_text)
+	#print(fic_list[0]) #debug
+	return preprocess_fics(fic_list)
 
-	return fic_text
+def save_to_csv(tagged_fics):
+	data = { 'Fic number': range(len(tagged_fics)),
+		'Sentence number': [len(fic) for fic in tagged_fics],
+	}
 
+	df = pandas.DataFrame(data, columns=['Fic number','Sentence number'])
+
+	df.to_csv('toy_tags.csv', index=False)
+
+def store_in_lists(tagged_sents):
+	### Store individual entities in lists ###
+	people=[] #Store entities tagged 'PERSON' here
+	locations=[] #Store entities tagged 'LOCATION' here
+	organizations=[] #Store entities tagged 'ORGANIZATION' Here
+	dates=[] #Store entities tagged 'DATE' Here
+	gpes=[] #Store entities tagged 'GPE' Here
+	facilities=[] #Store entities tagged 'FACILITY' Here
+
+	for chunk in tagged_sents.pos(): #result.pos() returns the leaves of the tree with their tags
+		if chunk[1] == 'PERSON':
+			#print(chunk[0],chunk[1]) #debug
+			people.append(chunk[0][0])
+
+		elif chunk[1] == 'LOCATION':
+			locations.append(chunk[0][0])
+	
+		elif chunk[1] == 'ORGANIZATION':
+			organizations.append(chunk[0][0])
+
+		elif chunk[1] == 'DATE':
+			dates.append(chunk[0][0])
+
+		elif chunk[1] == 'GPE':
+			gpes.append(chunk[0][0])
+
+		elif chunk[1] == 'FACILITY':
+			facilities.append(chunk[0][0])
 		
-### Open and get text from txt file
-proc_fic_text = get_processed_fanfics()
-#proc_fic_text=pos_tag(word_tokenize(proc_fic_text)) #debug
 
-### NER chunking ###
-result=nltk.ne_chunk(proc_fic_text) #Entity extraction
-
-
-#print(result.pos()) #debug
-
-### Store individual entities in lists ###
-people=[] #Store entities tagged 'PERSON' here
-locations=[] #Store entities tagged 'LOCATION' here
-organizations=[] #Store entities tagged 'ORGANIZATION' Here
-dates=[] #Store entities tagged 'DATE' Here
-gpes=[] #Store entities tagged 'GPE' Here
-facilities=[] #Store entities tagged 'FACILITY' Here
-
-for chunk in result.pos(): #result.pos() returns the leaves of the tree with their tags
-	if chunk[1] == 'PERSON':
-		#print(chunk[0],chunk[1]) #debug
-		people.append(chunk[0][0])
-
-	elif chunk[1] == 'LOCATION':
-		locations.append(chunk[0][0])
-
-	elif chunk[1] == 'ORGANIZATION':
-		organizations.append(chunk[0][0])
-
-	elif chunk[1] == 'DATE':
-		dates.append(chunk[0][0])
-
-	elif chunk[1] == 'GPE':
-		gpes.append(chunk[0][0])
-
-	elif chunk[1] == 'FACILITY':
-		facilities.append(chunk[0][0])
+	entities=people+locations+organizations+dates+gpes+facilities
+	print('PEOPLE: \n', sorted(set(people)))
+	print('LOCATIONS: \n', sorted(set(locations)))
+	print('ORGANIZATIONS: \n', sorted(set(organizations)))
+	print('DATES: \n', sorted(set(dates)))
+	print('GPES: \n', sorted(set(gpes)))
+	print('FACILITIES: \n', sorted(set(facilities)))
 	
 
-entities=people+locations+organizations+dates+gpes+facilities
-print('PEOPLE: \n', sorted(set(people)))
-print('LOCATIONS: \n', sorted(set(locations)))
-print('ORGANIZATIONS: \n', sorted(set(organizations)))
-print('DATES: \n', sorted(set(dates)))
-print('GPES: \n', sorted(set(gpes)))
-print('FACILITIES: \n', sorted(set(facilities)))
+	aux=open('./output.txt','w')
+	for element in entities:
+		aux.write(str(element)+'\n')
+
+	aux.close()
 
 
-aux=open('./output.txt','w')
-for element in entities:
-	aux.write(str(element)+'\n')
+### MAIN
+		
+### Open and get text from txt file
+processed_fics = get_processed_fanfics()
+#print(processed_fics[0]) #debug
+#print(type(processed_fics), type(processed_fics[0]), type(processed_fics[0][0]), type(processed_fics[0][0][0])) #debug
 
-aux.close()
 
-#print(result)
+### NER chunking ###
+tagged_fics = [[nltk.ne_chunk(sent) for sent in fic] for fic in processed_fics] #Entity extraction with NLTK NE chunker
+#print(tagged_fics[0]) #debug
+#print(tagged_fics[0].pos()) #debug
+
+
+### Save results to a csv file
+save_to_csv(tagged_fics)
+
+
 #result.draw() #requires Tkinter library
 
 
